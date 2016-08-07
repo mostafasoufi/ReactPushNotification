7a08c83af81c130bcbb4824e54189877c93c2382
milad davooabadi <persianfars@gmail.com>
2016-08-07 15:46:01 +0430

    firet Commit .

---
+++ ReactPushNotification.js
@@ -0,0 +1,52 @@
+/**
+ * @jsx React.DOM
+ *
+ */
+
+var Milad = React.createClass({
+    displayName: 'Notif',
+
+    getDefaultProps: function () {
+        return {
+            body: null,
+            icon: null
+        };
+    },
+
+    permission_request: function () {
+
+        return new Notification(this.props.message, {
+            body: this.props.body,
+            icon: this.props.icon
+        });
+    },
+
+    render: function () {
+        return (
+           <notification>
+               {this.permission_request()}
+           </notification>
+        );
+    }
+
+
+});
+
+function Notif(message, options) {
+
+
+    var props, wrapper;
+
+    if (options == null) {
+        options = {};
+    }
+
+    props = $.extend({
+        message: message
+    }, options);
+
+    wrapper = document.body.appendChild(document.createElement('div'));
+    React.render(<Milad {...props} />, wrapper);
+
+
+}
